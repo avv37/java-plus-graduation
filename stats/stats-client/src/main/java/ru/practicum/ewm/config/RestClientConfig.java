49e8ece47a97869b4ad7174ca8142e36e63d5a21
avv <avv37@yandex.com>
2026-01-16 15:25:19 +0300

    Изменение структуры проекта, настроены Spring Cloud Eureka, Config Server, Gateway.

--- stats/stats-client/src/main/java/ru/practicum/ewm/config/RestClientConfig.java
+++ stats/stats-client/src/main/java/ru/practicum/ewm/config/RestClientConfig.java
@@ -1,19 +1,13 @@
 package ru.practicum.ewm.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class RestClientConfig {
-    @Value("${stats-server.url}")
-    private String baseUrl;
-
     @Bean
-    public RestClient restClient(RestClient.Builder builder) {
-        return builder
-                .baseUrl(baseUrl)
-                .build();
+    public RestClient restClient() {
+        return RestClient.create();
     }
 }
